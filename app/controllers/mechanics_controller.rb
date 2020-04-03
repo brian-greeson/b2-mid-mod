54178301cc09f1b78962909e030d92d2baaa18ab
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-03 11:57:20 -0600

    Add update action to mechanic controller

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -8,8 +8,15 @@ class MechanicsController < ApplicationController
     @mechanic = Mechanic.find(mechanic_params[:mechanic_id])
   end
 
+  def update
+    @mechanic = Mechanic.find(mechanic_params[:mechanic_id])
+    @mechanic.rides << Ride.find(mechanic_params[:ride_id])
+
+    redirect_to "/mechanics/#{@mechanic.id}" 
+  end
+
 private
   def mechanic_params
-    params.permit(:mechanic_id)
+    params.permit(:mechanic_id, :ride_id)
   end
 end

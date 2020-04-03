04ee1c3ac52fb6587ef2d9d45eabbc7d0bca6ba5
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-03 11:56:43 -0600

    Add post route for mechanic update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get "/mechanics", to: "mechanics#index"
   get "/mechanics/:mechanic_id", to: "mechanics#show"
-
+  post "/mechanics/:mechanic_id", to: "mechanics#update"
+  
   get "/parks/:park_id", to: "parks#show"
 end

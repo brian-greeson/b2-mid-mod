8a134348920a5e21c899f7a50a880e48c844a465
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-03 11:45:07 -0600

    Update models with correct naming of joins table

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,6 @@ class Ride < ApplicationRecord
   validates_presence_of :name, :park_id
   validates :rating, inclusion: 1..10
   belongs_to :park
+  has_many :mechanic_rides
+  has_many :mechanics, through: :mechanic_rides
 end

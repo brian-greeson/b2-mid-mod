ff23037c2d790692a1688a807e0448814a50b36a
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-03 09:06:23 -0600

    Create features/mechanics test folders

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "when a user visits the mechanics index page: " do
+  it "See a header saying 'All Mechanics'" do
+
+  end
+  it "See a list of all mechanic’s names and their years of experience" do
+
+  end
+
+end
